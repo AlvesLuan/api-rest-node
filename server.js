0c6dff004120dd1f603e49982afd085a8f44faf7
Luan Santos <luankauan10@gmail.com>
2024-12-11 08:21:40 -0300

    init project

---
+++ server.js
@@ -0,0 +1,17 @@
+port = 3003;
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, ()=>{
+    /* console.log(`Servidor Iniciado na porta: $(port)`); */
+    console.log('Servidor Iniciado na porta: ',port);
+})
+
+app.get('/items', (request, response, next) =>{
+    response.send("Lista de Items!")
+
+})
