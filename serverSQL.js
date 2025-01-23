d764f2c31903283af1ad50965b4953b472951220
Luan K. Alves <luankauan10@gmail.com>
2025-01-22 21:21:22 -0300

    Create serverSQL.js

---
+++ serverSQL.js
@@ -0,0 +1,40 @@
+port = 3003;
+const express = require('express');
+const bd = require('./bancoDeDados')
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, ()=>{
+    console.log(`Servidor Iniciado na porta: ${port}`); 
+    /*console.log('Servidor Iniciado na porta: ',port); */
+})
+
+app.get('/items', (request, response, next) =>{
+    response.send(bd.listItems());
+})
+
+app.get('/items/:id', (request, response, next) =>{
+    response.send(bd.getItem(request.params.id));
+})
+
+
+app.post('/items',(request, response, next) =>{
+
+    const item = bd.createItem({
+        nome: request.body.nome,
+        valor: request.body.valor
+    })
+
+    response.send("posted")
+
+})
+
+RTCPeerConnection.connect(err =>{
+    if(err){
+        console.error('Erro ao conectar ao Banco de Dados:', err);
+        return;
+    }
+    console.log('Conexão bem-sucedida com o banco de dados! ');
+});
